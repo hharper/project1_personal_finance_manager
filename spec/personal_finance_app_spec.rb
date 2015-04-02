87d681b74dcea0b9330e543d08493cce3c2199d9
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-02 12:12:30 -0400

    added methods tests to RSPEC file

--- spec/personal_finance_app_spec.rb
+++ spec/personal_finance_app_spec.rb
@@ -1,3 +1,120 @@
+require_relative 'spec_helper'
+require_relative '/lib'
+# ^make sure this is the correct file structure...do i need actual files or is just directory of those files ok? Probably need to require each file...
+
+################
+##ACCOUNT TEST##
+################
+
+#pending test:#
+
+describe "account" do
+ it "has a balance"
+ it "has transactions"
+end
+
+#working test:#
+
+describe Account do
+  it "has a balance" do
+    account = Account.new(5000)
+    expect (account.balance).to eq(5000)
+  end
+  it "has transactions" do
+    account = Account.new
+    expect (account.transaction).to be_true
+  end
+end
+
+##################
+#TRANSACTION TEST#
+##################
+
+#pending test:#
+
+describe "transaction" do
+  it "belongs to one account"
+  it "has a date"
+  it "has a payee"
+  it "has a category"
+  it "has an amount"
+end
+
+#working test:#
+
+describe Transaction do
+  it "belongs to an account" do
+    transaction = Transaction.new(1)
+    expect (transaction.account_id).to eq(1)
+  end
+  it "has a date" do
+    transaction = Transaction.new(12/28/14)
+    expect (transaction.date).to eq(12/28/14)
+  end
+  it "has a payee" do
+    transaction = Transaction.new("Target")
+    expect (transaction.payee).to eq("Target")
+  end
+  it "has a category" do
+    transaction = Transaction.new("food")
+    expect (transaction.category).to eq("food")
+  end
+  it "has an amount" do
+    transaction = Transaction.new(100)
+    expect (transaction.amount).to eq(100)
+  end
+end
+
+
+###########################
+#INSTANCE METHODS TESTS#
+###########################
+describe "#update_balance" do
+
+  context "when the transaction is a credit (credit boolean == true)" do
+    it "adds the transaction amount to its account balance"
+    end
+
+  context "when the transaction is not a credit (credit boolean == false)" do
+    it "substracts the transaction amount from its account balance"
+  end
+
+end
+
+describe "#list_all_accounts" do
+  it "lists each account in the program with its corresponding balance"
+end
+
+describe "#get_account" do
+  it "gets the account id number from the user"
+end
+
+describe "#list_current_balance" do
+  it "lists the current balance of all accounts"
+end
+
+describe "#add_transaction" do
+  it "adds a transaction with all of user input into the transaction database, also updating the corresponding account balance with a new amount"
+end
+
+describe "#edit_transaction" do
+  it "allows the user to edit a specified transaction's information, also updating the corresponding account balance with a new amount"
+end
+
+describe "#remove_transaction" do
+  it "allows the user to delete a specified transaction, also updating the corresponding account balance with a new amount"
+end
+
+describe "#view_transaction_by_category" do
+  it "lists all transaction categories with their corresponding amount totals"
+end
+
+
+###########################
+#OTHER UNIMPLEMENTED TESTS#
+###########################
+
+
 describe "personal finance app" do
   it "has a user"
   it "has accounts it references"
@@ -9,20 +126,6 @@ describe "user" do
     user = User.new
     expect(user.account).to be_true
   end
-end
-
-describe "account" do
- it "belongs to a user"
- it "has transactions"
-end
-
-describe "transaction" do
-  it "belongs to one account"
-  it "belongs to a category"
-  it "has a date"
-  it "has a payee"
-  it "has a category"
-  it "has an amount"
 end
 
 describe "category" do
